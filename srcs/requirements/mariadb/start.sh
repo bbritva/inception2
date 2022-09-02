cc167e59d5ba321c4249417e37f115f40b3e9fe2
Boris <amgboris@ya.ru>
2022-09-02 12:26:17 +0200

    maria works

--- srcs/requirements/mariadb/start.sh
+++ srcs/requirements/mariadb/start.sh
@@ -2,19 +2,31 @@
 if [ ! -d /var/lib/mysql/${MYSQL_DB_NAME} ];
 then
     mysql_install_db --user=root --ldata=/var/lib/mysql
-    service mysql start
-    sleep 10
+    mysqld&
+    sleep 20
 
-    mariadb -u root -e "USE MYSQL; \
-    CREATE DATABASE IF NOT EXISTS  ${MYSQL_DB_NAME}; \
-    CREATE USER '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_USER_PASSWORD}'; \
-    GRANT ALL PRIVILEGES ON ${MYSQL_DB_NAME}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';\
-    CREATE USER '${MYSQL_ADMIN}'@'localhost' IDENTIFIED BY '${MYSQL_ADMIN_PASSWORD}'; \
-    GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_ADMIN}'@'%' IDENTIFIED BY '${MYSQL_ADMIN_PASSWORD}';\
-    SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}'); \
-    FLUSH PRIVILEGES;"
+    echo "CREATE DATABASE IF NOT EXISTS  ${MYSQL_DB_NAME};"
+    mariadb -u root -e "CREATE DATABASE IF NOT EXISTS  ${MYSQL_DB_NAME};"
 
-    service mysql stop
+    echo "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_USER_PASSWORD}';"
+    mariadb -u root -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_USER_PASSWORD}';"
+
+    echo "GRANT ALL PRIVILEGES ON ${MYSQL_DB_NAME}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+    mariadb -u root -e "GRANT ALL PRIVILEGES ON ${MYSQL_DB_NAME}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+
+    echo "CREATE USER '${MYSQL_ADMIN}'@'%' IDENTIFIED BY '${MYSQL_ADMIN_PASSWORD}';"
+    mariadb -u root -e "CREATE USER '${MYSQL_ADMIN}'@'%' IDENTIFIED BY '${MYSQL_ADMIN_PASSWORD}';"
+
+    echo "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_ADMIN}'@'%' IDENTIFIED BY '${MYSQL_ADMIN_PASSWORD}';"
+    mariadb -u root -e "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_ADMIN}'@'%' IDENTIFIED BY '${MYSQL_ADMIN_PASSWORD}';"
+
+    echo "SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}');"
+    mariadb -u root -e "SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}');"
+
+    echo "FLUSH PRIVILEGES;"
+    mariadb -u root -e "FLUSH PRIVILEGES;"
+    
+    killall mysqld
 fi
 
 exec mysqld
