1f695c6603fb16e7a27b21df83d734e5b77e8a65
Boris <amgboris@ya.ru>
2022-09-06 10:09:36 +0200

    wp werks but strange and bad

--- srcs/requirements/wordpress/start.sh
+++ srcs/requirements/wordpress/start.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-if [ ! -e /var/www/html/wordpress/wp-config.php ];
+if [ ! -e /var/www/html/wp-config.php ];
 then
+	sed -i -e 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 0.0.0.0:9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+	cd /var/www/html/
+	echo "wp core download --allow-root"
 	wp core download --allow-root
-	sleep 10
+	# sleep 10
+	echo "wp config create ${MYSQL_DB_NAME}"
 	wp config create \
 		--dbname=${MYSQL_DB_NAME} \
 		--dbuser=${MYSQL_USER} \
@@ -12,6 +16,8 @@ then
 		--dbhost=mariadb:3306 \
 		--locale=en_FR \
 		--allow-root
+	
+	echo "wp core install"	
 	wp core install \
 		--url=${DOMAIN_NAME} \
 		--title=${WP_TITLE} \
@@ -19,10 +25,14 @@ then
 		--admin_password=${WP_ADMIN_PASSWD} \
 		--admin_email=${WP_ADMIN_EMAIL} \
 		--allow-root
+
+	echo "wp user create $WP_USER_LOGIN $WP_USER_EMAIL"
 	wp user create $WP_USER_LOGIN $WP_USER_EMAIL \
 		--role=author \
 		--user_pass=$WP_USER_PASSWD \
 		--allow-root
+
+	echo "chown"
 	chown -R www-data:www-data /var/www/html/
 
 fi
